2de8ab8eb89893c04dd5b73b746ba75a26df6b3e
suryaKanthjunior <114057966+suryaKanthjunior@users.noreply.github.com>
2024-01-29 19:40:56 +0530

    payment and about us

--- src/pages/aboutUs/AboutUs.tsx
+++ src/pages/aboutUs/AboutUs.tsx
@@ -14,6 +14,185 @@ import ball from "../../assets/ball 3.png";
 import Layer3 from "../../assets/Layer_4.png";
 import Image1 from "../../assets/Rectangle 67.png";
 import PopupCarousel from "./PopupCarousel";
+import styled from "@emotion/styled";
+
+const StyledImage = styled.img`
+
+  @media (min-width: 300px) {
+    /* Extra small devices (phones) */
+    width: 370px;
+    height: 350px;
+  }
+
+  @media (min-width: 768px) {
+    /* Small devices (tablets) */
+    width: 370px;
+    height: 350px;
+  }
+
+  @media (min-width: 992px) {
+    /* Medium devices (desktops) */
+    width: 370px;
+    height: 350px;
+  }
+
+  @media (min-width: 1200px) {
+    /* Large devices (large desktops) */
+    width: 670px;
+    height: 846px;
+  }
+`;
+
+const StyledImage1 = styled.img`
+
+  @media (min-width: 300px) {
+    /* Extra small devices (phones) */
+    width: 120px;
+    height: 120px;
+    position: absolute;
+    transform: translate(205px, 285px);
+  }
+
+  @media (min-width: 768px) {
+    /* Small devices (tablets) */
+    width: 120px;
+    height: 120px;
+    position: absolute;
+    transform: translate(205px, 285px);
+  }
+
+  @media (min-width: 992px) {
+    /* Medium devices (desktops) */
+    width: 120px;
+    height: 120px;
+    position: absolute;
+    transform: translate(205px, 285px);
+  }
+
+  @media (min-width: 1200px) {
+    /* Large devices (large desktops) */
+    width: 300px;
+    height: 310px;
+    position: absolute;
+    transform: translate(345px, -45px);
+  }
+`;
+const StyledImage2 = styled.img`
+
+  @media (min-width: 300px) {
+    /* Extra small devices (phones) */
+    width: 50px;
+    height: 70px;
+    position: absolute;
+    transform: translate(81px, -27rem);
+  }
+
+  @media (min-width: 768px) {
+    /* Small devices (tablets) */
+    width: 50px;
+    height: 70px;
+    position: absolute;
+    transform: translate(81px, -27rem);
+  }
+
+  @media (min-width: 992px) {
+    /* Medium devices (desktops) */
+    width: 50px;
+    height: 70px;
+    position: absolute;
+    transform: translate(81px, -27rem);
+  }
+
+  @media (min-width: 1200px) {
+    /* Large devices (large desktops) */
+    width: 100px;
+    height: 137px;
+    position: absolute;
+    transform: translate(475px, -18rem);
+  }
+`;
+const StyledImage3 = styled.img`
+
+  @media (min-width: 300px) {
+    /* Extra small devices (phones) */
+    width: 100%;
+    margin-left: -30px;
+  }
+
+  @media (min-width: 768px) {
+    /* Small devices (tablets) */
+    width: 100%;
+    margin-left: -30px;
+  }
+
+  @media (min-width: 992px) {
+    /* Medium devices (desktops) */
+    width: 100%;
+    margin-left: -30px;
+  }
+
+  @media (min-width: 1200px) {
+    /* Large devices (large desktops) */
+    width: 100%;
+    margin-left: 5px;
+  }
+`;
+const StyledImage4 = styled.img`
+
+  @media (min-width: 300px) {
+    /* Extra small devices (phones) */
+    width: 85%;
+    height: 105%;
+  }
+
+  @media (min-width: 768px) {
+    /* Small devices (tablets) */
+    width: 85%;
+    height: 105%;
+  }
+
+  @media (min-width: 992px) {
+    /* Medium devices (desktops) */
+    width: 85%;
+    height: 105%;
+  }
+
+  @media (min-width: 1200px) {
+    /* Large devices (large desktops) */
+    width: 100%;
+    height: 99%;
+  }
+`;
+
+const StyledImage5 = styled.img`
+
+  @media (min-width: 300px) {
+    /* Extra small devices (phones) */
+    width: 159%;
+    margin-left: 37px;
+    margin-top: 12px;
+  }
+
+  @media (min-width: 768px) {
+    /* Small devices (tablets) */
+    width: 159%;
+    margin-left: 37px;
+    margin-top: 12px;
+  }
+
+  @media (min-width: 992px) {
+    /* Medium devices (desktops) */
+    width: 159%;
+    margin-left: 37px;
+    margin-top: 12px;
+  }
+
+  @media (min-width: 1200px) {
+    /* Large devices (large desktops) */
+    width: 100%;
+    margin: 1px;
+  }
+`;
 
 export default function AboutUs() {
   return (
@@ -34,13 +213,13 @@ export default function AboutUs() {
             justifyContent="center"
             alignItems="center"
             spacing={8}
+            flexDirection={{xs: "column", sm: "column", md: "column", lg: "row"}}
+            mb={{xs: "60px", sm: "60px", md: "60px", lg: "0px"}}
           >
-            <Grid item xs={5} justifyContent="center" alignItems="center">
-              <Typography
+            <Grid item xs={0} pl={{xs: '58px', sm: "58px", md: "58px", lg: "70px"}} justifyContent="center" alignItems="center">
+              <Typography pt={{xs: '25px', sm: "25px", md: "25px", lg: "0px"}} marginRight={{xs: "66px", sm: "66px", md: "66px", lg: "0px"}} fontSize={{xs: "15px", sm: "15px", md: "15px", lg: "20px"}} maxWidth={{xs: "298px", sm: "298px", md: "298px", lg: "438px"}}
                 style={{
-                  maxWidth: "438px",
                   height: "284px",
-                  fontSize: "20px",
                   fontStyle: "normal",
                   fontWeight: "700",
                 }}
@@ -52,21 +231,13 @@ export default function AboutUs() {
                 establish a haven where people could gather, connect, and stay
                 active.
               </Typography>
-              <img
+              <StyledImage1
                 src={Ball4}
-                height={310}
-                width={300}
                 alt="ball 4"
-                style={{
-                  position: "absolute",
-                  transform: `translate(345px, -45px)`,
-                }}
               />
             </Grid>
-            <Grid item xs={7}>
-              <img
-                height="846px"
-                style={{ width: "100%", minWidth: "670px" }}
+            <Grid item xs={7} display={{xs: 'contents', sm: "contents", md: "contents", lg: "block"}}>
+              <StyledImage
                 src={Aboutus}
                 alt={"title"}
               />
@@ -84,16 +255,19 @@ export default function AboutUs() {
           container
           maxWidth={1200}
           margin="auto"
-          paddingY={12}
-          paddingX={5}
+         pt={{xs: "70px", sm: '70px', md: '70px', lg: "96px"}}
+         pb={{xs: "60px", sm: '60px', md: '60px', lg: "96px"}}
+          pl={{xs: "0px", sm: "0px", md: "0px", lg: "40px"}}
+          pr={{xs: "15px", sm: "15px", md: "15px", lg: "40px"}}
           columnSpacing={4}
+          flexDirection={{xs: "column", sm: "column", md: "column", lg: "row"}}
+          gap={{xs: "30px", sm: "30px", md: "30px", lg: "0px"}}
         >
-          <Grid item xs={6}>
+          <Grid item xs={6} maxWidth={{xs: "100%", sm: "100%", md: "100%", lg: "50%"}}>
             <Box>
-              <Typography
+              <Typography fontSize={{xs: '20px', sm: '20px', md: '20px', lg: "32px"}}
                 variant={"h4"}
                 style={{
-                  fontSize: "32px",
                   fontStyle: "normal",
                   fontWeight: "700",
                   marginBottom: "10px",
@@ -101,9 +275,8 @@ export default function AboutUs() {
               >
                 Mission
               </Typography>
-              <Typography
+              <Typography fontSize={{xs: '15px', sm: '15px', md: '15px', lg: "20px"}}
                 style={{
-                  fontSize: "20px",
                   fontStyle: "normal",
                   fontWeight: "400",
                   marginRight: "20px",
@@ -118,12 +291,11 @@ export default function AboutUs() {
               </Typography>
             </Box>
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={6} maxWidth={{xs: "100%", sm: "100%", md: "100%", lg: "50%"}}>
             <Box>
-              <Typography
+              <Typography fontSize={{xs: '20px', sm: '20px', md: '20px', lg: "32px"}}
                 variant={"h4"}
                 style={{
-                  fontSize: "32px",
                   fontStyle: "normal",
                   fontWeight: "700",
                   marginBottom: "10px",
@@ -131,12 +303,11 @@ export default function AboutUs() {
               >
                 Vision
               </Typography>
-              <Typography
+              <Typography fontSize={{xs: '15px', sm: '15px', md: '15px', lg: "20px"}}
                 style={{
-                  fontSize: "20px",
                   fontStyle: "normal",
                   fontWeight: "400",
-                  marginRight: "20px",
+                  marginRight: "27px",
                 }}
                 variant={"body1"}
               >
@@ -147,15 +318,9 @@ export default function AboutUs() {
                 the delight of sports and recreation is within everyone's reach.
               </Typography>
             </Box>
-            <img
+            <StyledImage2
               src={Layer3}
-              width={100}
-              height={137}
               alt="layer3"
-              style={{
-                position: "absolute",
-                transform: `translate(475px, -18rem)`,
-              }}
             />
           </Grid>
         </Grid>
@@ -166,25 +331,27 @@ export default function AboutUs() {
             PlayZo Facilities
           </Typography>
           <Grid container>
-            <Stack direction="row" spacing={3}>
-              <Grid item xs={6}>
+            <Stack alignItems={{xs: "center", sm: "center", md: "center", lg: "normal"}} direction={{xs: "column", sm: "column", md: "column", lg: "row"}} spacing={3}>
+              <Grid item xs={6} maxWidth={{xs: '83%', sm: "83%", md: "83%", lg: '50%'}} margin={{xs: "30px 30px", sm: "30px 30px", md: "30px 30px", lg: "0px"}}>
                 <img src={turf} alt="grid 1" style={{ width: "100%" }} />
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={6} maxWidth={{xs: '83%', sm: "83%", md: "83%", lg: '50%'}} margin={{xs: "-23px 30px", sm: "-23px 30px", md: "-23px 30px", lg: "0px"}}>
                 <img src={turf1} alt="grid 2" style={{ width: "100%" }} />
               </Grid>
             </Stack>
           </Grid>
           <Grid container>
-            <Stack direction="row" spacing={3}>
-              <Grid item xs={3}>
-                <img src={turf2} alt="grid 3" style={{ width: "100%" }} />
+            <Stack direction="row" marginBottom={{xs: "80px", sm: "80px", md: "80px", lg: '0px'}} spacing={{xs: 0, sm: 0, md: 0, lg: 3}}>
+              <Box alignItems={{xs: "center", sm: "center", md: "center", lg: "normal"}} gap={{xs: "0px", sm: "0px", md: "0px", lg: "25px"}} display={{xs: "flex", sm: "flex", md: "flex", lg: "flex"}} flexDirection={{xs: 'column', sm: "column", md: "column", lg: "row"}}>
+              <Grid item xs={0} ml={{xs: "20px", sm: "20px", md: "20px", lg: "0px"}} width={{xs: '73%', sm: "73%", md: "73%", lg: '37%'}}>
+                <StyledImage4 src={turf2} alt="grid 3" />
               </Grid>
-              <Grid item xs={5}>
-                <img src={turf3} alt="grid 4" style={{ width: "100%" }} />
+              <Grid item xs={0}>
+                <StyledImage5 src={turf3} alt="grid 4" />
               </Grid>
-              <Grid item xs={4}>
-                <img src={turf4} alt="grid 5" style={{ width: "100%" }} />
+              </Box>
+              <Grid item xs={0} width={{xs: '135%', sm: "135%", md: "135%", lg: '33%'}}>
+                <StyledImage3 src={turf4} alt="grid 5" />
               </Grid>
             </Stack>
           </Grid>
@@ -210,9 +377,9 @@ export default function AboutUs() {
             </Link>
             <EastIcon sx={{ color: Colors.BUTTON_COLOR, fontSize: "35px" }} />
           </Stack>
-          <Box paddingY={5}>
-            <Grid container spacing={5}>
-              <Grid item xs={12}>
+          <Box pb={{xs: "0px", sm: "0px", md: "0px", lg: "40px"}} >
+            <Grid  container spacing={5}>
+              <Grid  item xs={12} marginTop={{xs: "-82px", sm: "-49px", md: "-49px", lg: "40px"}} marginLeft={{xs: "16px", sm: "16px", md: "16px", lg: "0px"}}>
                 <img
                   src={ball}
                   width={132}
@@ -223,7 +390,7 @@ export default function AboutUs() {
                     transform: `translate(135px, -85px)`,
                   }}
                 />
-                <Typography
+                <Typography maxWidth={{xs: "200px", sm: "200px", md: "200px", lg: "441px"}} fontSize={{xs: "15px", sm: "15px", md: "15px", lg: "20px"}}
                   variant="h6"
                   color={Colors.BUTTON_COLOR}
                   fontWeight={600}
