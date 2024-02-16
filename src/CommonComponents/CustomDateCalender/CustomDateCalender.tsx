019edf47f327c2b5588dc2ce5726ed4821ee9029
Dinesh <djeeva1515@gmail.com>
2024-02-16 16:32:20 +0530

    booking

--- src/CommonComponents/CustomDateCalender/CustomDateCalender.tsx
+++ src/CommonComponents/CustomDateCalender/CustomDateCalender.tsx
@@ -9,18 +9,12 @@ import DateUtils from "../../Utils/DateUtils";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import ModalComponent from "./ModalComponent";
 import leftarrow from "./left-arrow.svg";
-import moment from 'moment';
+import moment from "moment";
 import rightarrow from "./right-arrow.svg";
 import routes from "../../routes/routes";
 import { useNavigate } from "react-router-dom";
 
 // import { BookingType } from "../../CommonFiles/BookingType";
-
-
-
-
-
-
 
 const Timings = [
   { name: "6:00-7:00 AM" },
@@ -50,7 +44,6 @@ interface TimeSlot {
   endTime: string;
 }
 
-
 interface CustomDateCalendarProps {
   tableData?: any;
   setTableData?: any;
@@ -60,9 +53,9 @@ interface CustomDateCalendarProps {
 }
 
 interface datatype {
-  startTime: number,
-  endTime: number,
-  type: String,
+  startTime: number;
+  endTime: number;
+  type: String;
 }
 export default function CustomDateCalendar({
   tableData,
@@ -72,7 +65,7 @@ export default function CustomDateCalendar({
   selectedService,
 }: CustomDateCalendarProps) {
   const [selectedDate, setSelectedDate] = React.useState<string>("");
-  const user = localStorage.getItem('user');
+  const user = localStorage.getItem("user");
   const userData = user && JSON.parse(user);
 
   const CustomDateHeader = (props: any) => {
@@ -122,8 +115,6 @@ export default function CustomDateCalendar({
   //   }
   // };
 
-
-
   const [selectedTimings, setSelectedTimings] = React.useState<string[]>([]);
   const navigate = useNavigate();
   const [modalOpen, setModalOpen] = React.useState(false);
@@ -170,27 +161,42 @@ export default function CustomDateCalendar({
 
   const handleClose = () => {
     setModalOpen(false);
-    navigate(routes.ROOT)
-  }
+    navigate(routes.ROOT);
+  };
 
   const handleDateSelection = (newValue: any) => {
-
     let datedata = newValue.$d;
     const parsedDate = moment(datedata);
-    const formattedDate = parsedDate.format('YYYY-MM-DD');
+    const formattedDate = parsedDate.format("YYYY-MM-DD");
 
     setSelectedDate(formattedDate);
     ApiCall(formattedDate);
-
   };
 
-  const data = [{ startTime: 1709001000000, endTime: 1709008200000, type: 'badminton' }];
-  const data2 = [{ startTime: 1708032600000, endTime: 1708047000000, type: "cricketNet" }];
-  const data3 = [{ startTime: 1707438600000, endTime: 1707445800000, type: 'boardGame' }];
-  const data4 = [{ startTime: 1709245800000, endTime: 1709253000000, type: 'turf' }];
-  const data5 = [{ startTime: 1708482600000, endTime: 1708489800000, type: 'bowlingMachine' },
-  { startTime: 1708497000000, endTime: 1708504200000, type: 'bowlingMachine' }];
-
+  const data = [
+    { startTime: 1709001000000, endTime: 1709008200000, type: "badminton" },
+  ];
+  const data2 = [
+    { startTime: 1708032600000, endTime: 1708047000000, type: "cricketNet" },
+  ];
+  const data3 = [
+    { startTime: 1707438600000, endTime: 1707445800000, type: "boardGame" },
+  ];
+  const data4 = [
+    { startTime: 1709245800000, endTime: 1709253000000, type: "turf" },
+  ];
+  const data5 = [
+    {
+      startTime: 1708482600000,
+      endTime: 1708489800000,
+      type: "bowlingMachine",
+    },
+    {
+      startTime: 1708497000000,
+      endTime: 1708504200000,
+      type: "bowlingMachine",
+    },
+  ];
 
   const ApiCall = async (dateValue: any) => {
     try {
@@ -202,10 +208,9 @@ export default function CustomDateCalendar({
       setDisableData(data2);
       MilisecondsToHours();
     } catch (error: any) {
-      console.error('Error:', error.message);
+      console.error("Error:", error.message);
     }
   };
-
 
   const MilisecondsToHours = () => {
     if (disableData.length > 0) {
@@ -213,13 +218,13 @@ export default function CustomDateCalendar({
         const value1 = convertTo24HourFormat(item.startTime);
         const value2 = convertTo24HourFormat(item.endTime);
 
-        const TimeSlot = splitTimeRange(value1, value2)
+        const TimeSlot = splitTimeRange(value1, value2);
 
         console.log("startTime", value1, value2, TimeSlot);
         return;
-      })
+      });
     }
-  }
+  };
 
   const convertTo24HourFormat = (milliseconds: number): string => {
     const date = new Date(milliseconds);
@@ -227,17 +232,17 @@ export default function CustomDateCalendar({
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
 
-    const formattedHours = hours.toString().padStart(2, '0');
-    const formattedMinutes = minutes.toString().padStart(2, '0');
-    const formattedSeconds = seconds.toString().padStart(2, '0');
+    const formattedHours = hours.toString().padStart(2, "0");
+    const formattedMinutes = minutes.toString().padStart(2, "0");
+    const formattedSeconds = seconds.toString().padStart(2, "0");
 
     return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
   };
 
   const splitTimeRange = (startTime: string, endTime: string): TimeSlot[] => {
     const timeSlots: TimeSlot[] = [];
-    const startHour = parseInt(startTime.split(':')[0], 10);
-    const endHour = parseInt(endTime.split(':')[0], 10);
+    const startHour = parseInt(startTime.split(":")[0], 10);
+    const endHour = parseInt(endTime.split(":")[0], 10);
 
     for (let hour = startHour; hour < endHour; hour++) {
       const startSlot = `${hour}:00`;
@@ -252,15 +257,27 @@ export default function CustomDateCalendar({
     if (disableData) {
       MilisecondsToHours();
     }
-  }, [])
+  }, []);
 
-  console.log('date', selectedDate, type, disableData);
+  console.log("date", selectedDate, type, disableData);
 
   const handleTimeSelection = (time: string) => {
-    setSelectedTimings((prevSelectedTimings) => [...prevSelectedTimings, time]);
+    setSelectedTimings((prevSelectedTimings) => {
+      // If the time is already selected, remove it; otherwise, add it
+      if (prevSelectedTimings.includes(time)) {
+        return prevSelectedTimings.filter(
+          (selectedTime) => selectedTime !== time
+        );
+      } else {
+        return [...prevSelectedTimings, time];
+      }
+    });
   };
 
-  const handleAddButtonClick = async (type: string, selectedService: string) => {
+  const handleAddButtonClick = async (
+    type: string,
+    selectedService: string
+  ) => {
     if (userData && userData.userType === "user") {
       if (selectedDate !== "" && selectedTimings.length > 0) {
         const totalDuration = selectedTimings.length;
@@ -277,72 +294,68 @@ export default function CustomDateCalendar({
           // ... other properties
         };
 
-
-    try {
-      const response = await BookingApi.getBookingAmount(bookings.type);
-      if (response) {
-        console.log("response",response);
-        ratePerHour = response.bookingAmount;
-        bookings.amount = totalDuration * ratePerHour;
-      } else {
-        console.log('Booking Failed');
-      }
-    } catch (err) {
-      console.log("err",err)
-    }
-    console.log("selectedTimings",selectedTimings)
-    if(selectedTimings.length>0){
-      selectedTimings.map(async (timeData)=>{
-try {
-  let startMilliseconds = 0;
-  let endMilliseconds = 0;
-  const [time, am] = timeData.split(" ");
-  const [time1, am1] = time.split("-");
-  const [hours, minutes] = time1.split(":");
-  console.log("hours",hours,minutes)
-        const startDateTime = new Date(selectedDate);
-        startDateTime.setHours(Number(hours), Number(minutes));
-        startMilliseconds = startDateTime.getTime(); // Start time in milliseconds
-
-        const endDateTime = new Date(startDateTime);
-        endDateTime.setHours(startDateTime.getHours() + 1); // Adding 1 hour, you can adjust this based on your requirement
-        endMilliseconds = endDateTime.getTime(); // End time in milliseconds
-    await BookingApi.getBookedList({
-          type: bookings.type,
-          bookingtype: "online",
-          startTime: startMilliseconds,
-          endTime: endMilliseconds,
-          user: userData.id,
-          startDate: DateUtils.formatDate(new Date(selectedDate),"YYYY-MM-DD"),
-          endDate: DateUtils.formatDate(new Date(selectedDate),"YYYY-MM-DD"),
+        try {
+          const response = await BookingApi.getBookingAmount(bookings.type);
+          if (response) {
+            console.log("response", response);
+            ratePerHour = response.bookingAmount;
+            bookings.amount = totalDuration * ratePerHour;
+          } else {
+            console.log("Booking Failed");
           }
-        );
-  
-    } catch (err) {
-      console.log("err",err)
-    }
+        } catch (err) {
+          console.log("err", err);
+        }
+        console.log("selectedTimings", selectedTimings);
+        if (selectedTimings.length > 0) {
+          selectedTimings.map(async (timeData) => {
+            try {
+              let startMilliseconds = 0;
+              let endMilliseconds = 0;
+              const [time, am] = timeData.split(" ");
+              const [time1, am1] = time.split("-");
+              const [hours, minutes] = time1.split(":");
+              console.log("hours", hours, minutes);
+              const startDateTime = new Date(selectedDate);
+              startDateTime.setHours(Number(hours), Number(minutes));
+              startMilliseconds = startDateTime.getTime(); // Start time in milliseconds
 
+              const endDateTime = new Date(startDateTime);
+              endDateTime.setHours(startDateTime.getHours() + 1); // Adding 1 hour, you can adjust this based on your requirement
+              endMilliseconds = endDateTime.getTime(); // End time in milliseconds
+              await BookingApi.getBookedList({
+                type: bookings.type,
+                bookingtype: "online",
+                startTime: startMilliseconds,
+                endTime: endMilliseconds,
+                // user: userData.id,
+                user: 1,
 
+                startDate: DateUtils.formatDate(
+                  new Date(selectedDate),
+                  "YYYY-MM-DD"
+                ),
+                endDate: DateUtils.formatDate(
+                  new Date(selectedDate),
+                  "YYYY-MM-DD"
+                ),
+              });
+            } catch (err) {
+              console.log("err", err);
+            }
+          });
+          setTableData((prevTableData: any) => [...prevTableData, bookings]);
 
-      })
-      setTableData((prevTableData: any) => [...prevTableData, bookings]);
-
-      // Reset selected date and timings
-      setSelectedDate("");
-      setSelectedTimings([]);
-
-
-    }
-  
-  }
-}else{
-  setModalOpen(true);
-
-  navigate(routes.ROOT)
-
+          // Reset selected date and timings
+          setSelectedDate("");
+          setSelectedTimings([]);
+        }
       }
-      
+    } else {
+      setModalOpen(true);
 
+      navigate(routes.ROOT);
+    }
   };
 
   return (
@@ -415,8 +428,8 @@ try {
                 border: item.disabled
                   ? "1px solid #9C9C9C"
                   : selectedTimings.includes(item.name)
-                    ? "2px solid #15B5FC"
-                    : "1px solid black",
+                  ? "2px solid #15B5FC"
+                  : "1px solid black",
                 textAlign: "center",
                 padding: "4px 0px 5px 0px",
                 display: "flex",
@@ -432,8 +445,8 @@ try {
                     item.disabled
                       ? "#9C9C9C"
                       : selectedTimings.includes(item.name)
-                        ? "#15B5FC"
-                        : "black"
+                      ? "#15B5FC"
+                      : "black"
                   }
                   fontSize={{ xs: "14px", sm: "14px", md: "14px", lg: "18px" }}
                 >
@@ -460,5 +473,3 @@ try {
     </Stack>
   );
 }
-
-
