0663ff92f6e64bf830e89b03d96be97e27bfa767
Dinesh <djeeva1515@gmail.com>
2024-03-22 18:51:16 +0530

    removed unwanted codes and added number of persons in user profile

--- src/pages/user/ListCard.tsx
+++ src/pages/user/ListCard.tsx
@@ -32,18 +32,6 @@ export default function ListCard({ userDetails, userName }: dataProps) {
                 {user.id}
               </Typography>
             </Stack>
-
-            {/* <Rating
-              name="size-small"
-              size="small"
-              value={user.ratings}
-              readOnly
-              sx={{
-                ".css-1c99szj-MuiRating-icon": {
-                  color: `${Colors.BUTTON_COLOR}`,
-                },
-              }}
-            /> */}
           </Stack>
           <Typography variant="body2" fontWeight={600}>
             {DateUtils.formatDate(user.startDate, "DD-MMM-YYYY")}
@@ -52,13 +40,17 @@ export default function ListCard({ userDetails, userName }: dataProps) {
             direction={{ xs: "column", sm: "row", md: "row", lg: "row" }}
             spacing={{ xs: 2, sm: 5, md: 5, lg: 5 }}
             alignItems="start"
-            // flexWrap={"wrap"}
           >
             <Box display={"flex"} justifyContent={"space-between"} gap={"2rem"}>
               <Typography variant="body2">{userName}</Typography>
               <Typography variant="body2">
                 {CapitalizeFirstLetter(user.type)}
               </Typography>
+              {user.type === "Badminton" ? (
+                <Typography>/{user.numberOfPersons}</Typography>
+              ) : (
+                ""
+              )}
             </Box>
             <Box display={"flex"} justifyContent={"space-between"} gap={"2rem"}>
               <Typography variant="body2">
@@ -75,96 +67,13 @@ export default function ListCard({ userDetails, userName }: dataProps) {
                 user.endTime
               )}`}</Typography>
             </Box>
-            {/* <Button
-                variant="outlined"
-                sx={{
-                  padding: "8px 20px",
-                  textTransform: "none",
-                  fontSize: "16px",
-                  minWidth: "110px",
-                  fontWeight: "400",
-                  border: "2px solid #15B5FC",
-                  borderRadius: "30px",
-                  // letterSpacing: "1.6px",
-                  background: Colors.BUTTON_COLOR,
-                  color: Colors.WHITE,
-                  "&:hover": {
-                    background: Colors.WHITE,
-                    color: Colors.BUTTON_COLOR,
-                    border: "2px solid #15B5FC",
-                  },
-                }}
-              >
-               Booking cancelled
-              </Button> */}
           </Stack>
           <Stack
             direction="row"
             spacing={1}
             alignItems="flex-end"
             justifyContent="flex-end"
-            // sx={{
-            //   display: "flex",
-            //   flexWrap: "wrap", // Add flexWrap property
-            //   justifyContent: "space-between",
-            // }}
           >
-            {/* {!user.comments? (
-              <Stack direction="row" spacing={1} alignItems="center">
-                <img src={Vector} width={15} height={15} />
-                <Typography variant="body2">Add comment</Typography>
-              </Stack>
-            ) : (
-              <Stack direction="row" spacing={1} alignItems="baseline">
-                <img src={Vector} width={15} height={15} />
-                <Typography variant="body2" maxWidth={360} width={"100%"}>
-                  {user.comments}
-                </Typography>
-              </Stack>
-            )} */}
-
-            {/* {user.bookingCompleted === "yes" ? (
-              <Stack direction="row" spacing={1} alignItems="center">
-                <img src={Vector1} width={13} height={9} />
-                <Typography variant="body2">Booking completed</Typography>
-              </Stack>
-            ) : user.bookingCompleted === "no" &&
-              user.refundCompleted === "yes" ? (
-              <Stack
-                direction={{ xs: "column", md: "row" }}
-                spacing={3}
-                alignItems="center"
-              >
-                <Stack direction="row" spacing={1} alignItems="center">
-                  <img src={Vector1} width={13} height={9} />
-                  <Typography variant="body2">Booking cancelled</Typography>
-                </Stack>
-                <Stack direction="row" spacing={1} alignItems="center">
-                  <img src={Vector1} width={13} height={9} />
-                  <Typography variant="body2">Refund completed</Typography>
-                </Stack>
-              </Stack>
-            ) : (
-              <Stack
-                direction={{ xs: "column", md: "row" }}
-                spacing={3}
-                alignItems="center"
-                sx={{
-                  display: "flex",
-
-                  flexWrap: "wrap", // Add flexWrap property
-                }}
-              >
-                <Stack direction="row" spacing={1} alignItems="center">
-                  <img src={Vector1} width={13} height={9} />
-                  <Typography variant="body2">Booking cancelled</Typography>
-                </Stack>
-                <Stack direction="row" spacing={1} alignItems="center">
-                  <img src={Vector2} width={9} height={10} />
-                  <Typography variant="body2">Refund initiated</Typography>
-                </Stack>
-              </Stack>
-            )} */}
             <Stack
               direction={{ xs: "column", md: "row" }}
               spacing={3}
@@ -176,8 +85,6 @@ export default function ListCard({ userDetails, userName }: dataProps) {
               }}
             >
               <Stack direction="row" spacing={1} alignItems="center">
-                {/* <img src={Vector2} width={9} height={10} />
-                  <Typography variant="body2">Refund initiated</Typography> */}
                 <Button
                   variant="outlined"
                   sx={{
