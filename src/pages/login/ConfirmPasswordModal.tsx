0bd030484f9245a7ea30cdf179f6b57f81c07abb
Dinesh <djeeva1515@gmail.com>
2024-03-22 16:46:19 +0530

    added success modal

--- src/pages/login/ConfirmPasswordModal.tsx
+++ src/pages/login/ConfirmPasswordModal.tsx
@@ -13,6 +13,7 @@ import { Visibility, VisibilityOff } from "@mui/icons-material";
 import CloseIcon from "@mui/icons-material/Close";
 import Colors from "../../CommonComponents/Colors";
 import CustomLabel from "../../CommonComponents/CustomLabel";
+import SucessfullyModal from "./SucessModal";
 import UserApi from "../../api/UserApi";
 import { useState } from "react";
 
@@ -36,6 +37,7 @@ export default function ConfirmPasswordModal({
   const [passwordError, setPasswordError] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [sucess, setSucess] = useState(false);
 
   const handleConfirmClose = () => {
     // Check if both password fields are not empty
@@ -62,6 +64,7 @@ export default function ConfirmPasswordModal({
           setShowOTPModal(false); // Close the ShowOTPModal
           setOpenForgetModal(false); // Close the OpenForgetModal
           handleClose();
+          setSucess(true);
         })
         .catch((error) => {
           alert(error);
@@ -82,152 +85,159 @@ export default function ConfirmPasswordModal({
   };
 
   return (
-    <Dialog
-      open={open}
-      maxWidth="xs"
-      fullWidth
-      sx={{
-        borderRadius: "42px !important",
-        "& .MuiDialog-paper": {
+    <>
+      <Dialog
+        open={open}
+        maxWidth="xs"
+        fullWidth
+        sx={{
           borderRadius: "42px !important",
-        },
-      }}
-    >
-      <DialogContent
-        style={{
-          background: Colors.BACKGROUND_COLOR,
+          "& .MuiDialog-paper": {
+            borderRadius: "42px !important",
+          },
         }}
       >
-        <Stack direction="row" justifyContent="end">
-          <IconButton onClick={handleClose}>
-            <CloseIcon sx={{ color: Colors.WHITE, fontSize: "30px" }} />
-          </IconButton>
-        </Stack>
-        <Stack direction="column" spacing={3} padding={2}>
-          <Box>
-            <CustomLabel color={Colors.WHITE} mb={1}>
-              New Password
-            </CustomLabel>
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              sx={{
-                backgroundColor: Colors.WHITE,
-                width: "100%",
-                borderRadius: "8px",
-                "& .MuiOutlinedInput-root": {
-                  borderRadius: "8px",
-                },
-              }}
-              placeholder="Enter new Password"
-              required
-              type={showPassword ? "text" : "password"}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-
-            <CustomLabel mt={2} color={Colors.WHITE} mb={1}>
-              Confirm Password
-            </CustomLabel>
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              sx={{
-                backgroundColor: Colors.WHITE,
-                width: "100%",
-                borderRadius: "8px",
-                "& .MuiOutlinedInput-root": {
-                  borderRadius: "8px",
-                },
-              }}
-              placeholder="Enter Confirm Password"
-              required
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              type={showConfirmPassword ? "text" : "password"}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowConfirmPassword}
-                      edge="end"
-                    >
-                      {showConfirmPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-            {passwordError && (
-              <CustomLabel mt={1} color="red">
-                {passwordError}
+        <DialogContent
+          style={{
+            background: Colors.BACKGROUND_COLOR,
+          }}
+        >
+          <Stack direction="row" justifyContent="end">
+            <IconButton onClick={handleClose}>
+              <CloseIcon sx={{ color: Colors.WHITE, fontSize: "30px" }} />
+            </IconButton>
+          </Stack>
+          <Stack direction="column" spacing={3} padding={2}>
+            <Box>
+              <CustomLabel color={Colors.WHITE} mb={1}>
+                New Password
               </CustomLabel>
-            )}
-          </Box>
+              <TextField
+                id="outlined-basic"
+                variant="outlined"
+                sx={{
+                  backgroundColor: Colors.WHITE,
+                  width: "100%",
+                  borderRadius: "8px",
+                  "& .MuiOutlinedInput-root": {
+                    borderRadius: "8px",
+                  },
+                }}
+                placeholder="Enter new Password"
+                required
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
 
-          <Button
-            type="button"
-            sx={{
-              padding: "12px 20px",
-              textTransform: "none",
-              fontSize: "16px",
-              minWidth: "110px",
-              fontWeight: "400",
-              border: "2px solid #15B5FC",
-              borderRadius: "50px",
-              letterSpacing: "1.6px",
-              background: Colors.BUTTON_COLOR,
-              color: Colors.WHITE,
-              ":hover": {
-                background: Colors.WHITE,
-                color: Colors.BUTTON_COLOR,
-                border: "2px solid #15B5FC",
-              },
-            }}
-            onClick={handleConfirmClose}
-          >
-            Confirm
-          </Button>
+              <CustomLabel mt={2} color={Colors.WHITE} mb={1}>
+                Confirm Password
+              </CustomLabel>
+              <TextField
+                id="outlined-basic"
+                variant="outlined"
+                sx={{
+                  backgroundColor: Colors.WHITE,
+                  width: "100%",
+                  borderRadius: "8px",
+                  "& .MuiOutlinedInput-root": {
+                    borderRadius: "8px",
+                  },
+                }}
+                placeholder="Enter Confirm Password"
+                required
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                type={showConfirmPassword ? "text" : "password"}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowConfirmPassword}
+                        edge="end"
+                      >
+                        {showConfirmPassword ? (
+                          <Visibility />
+                        ) : (
+                          <VisibilityOff />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              {passwordError && (
+                <CustomLabel mt={1} color="red">
+                  {passwordError}
+                </CustomLabel>
+              )}
+            </Box>
 
-          <Button
-            type="button"
-            sx={{
-              padding: "12px 20px",
-              textTransform: "none",
-              fontSize: "16px",
-              minWidth: "110px",
-              fontWeight: "400",
-              border: "2px solid #15B5FC",
-              borderRadius: "50px",
-              letterSpacing: "1.6px",
-              background: Colors.BUTTON_COLOR,
-              color: Colors.WHITE,
-              ":hover": {
-                background: Colors.WHITE,
-                color: Colors.BUTTON_COLOR,
+            <Button
+              type="button"
+              sx={{
+                padding: "12px 20px",
+                textTransform: "none",
+                fontSize: "16px",
+                minWidth: "110px",
+                fontWeight: "400",
                 border: "2px solid #15B5FC",
-              },
-            }}
-            onClick={handleClose}
-          >
-            Back
-          </Button>
-        </Stack>
-      </DialogContent>
-    </Dialog>
+                borderRadius: "50px",
+                letterSpacing: "1.6px",
+                background: Colors.BUTTON_COLOR,
+                color: Colors.WHITE,
+                ":hover": {
+                  background: Colors.WHITE,
+                  color: Colors.BUTTON_COLOR,
+                  border: "2px solid #15B5FC",
+                },
+              }}
+              onClick={handleConfirmClose}
+            >
+              Confirm
+            </Button>
+
+            <Button
+              type="button"
+              sx={{
+                padding: "12px 20px",
+                textTransform: "none",
+                fontSize: "16px",
+                minWidth: "110px",
+                fontWeight: "400",
+                border: "2px solid #15B5FC",
+                borderRadius: "50px",
+                letterSpacing: "1.6px",
+                background: Colors.BUTTON_COLOR,
+                color: Colors.WHITE,
+                ":hover": {
+                  background: Colors.WHITE,
+                  color: Colors.BUTTON_COLOR,
+                  border: "2px solid #15B5FC",
+                },
+              }}
+              onClick={handleClose}
+            >
+              Back
+            </Button>
+          </Stack>
+        </DialogContent>
+      </Dialog>
+      <SucessfullyModal open={sucess} handleClose={() => setSucess(false)} />
+    </>
   );
 }
